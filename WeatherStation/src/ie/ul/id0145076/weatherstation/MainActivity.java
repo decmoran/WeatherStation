eb9d6eb35001733cb6b272922b80a876650d3c99
decmoran <0145076@studentmail.ul.ie>
2015-04-29 13:55:09 +0100

    Added menu Items

--- WeatherStation/src/ie/ul/id0145076/weatherstation/MainActivity.java
+++ WeatherStation/src/ie/ul/id0145076/weatherstation/MainActivity.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -22,6 +23,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -48,6 +50,7 @@ public class MainActivity extends Activity implements SensorEventListener, Locat
 	private TextView viewPressure;
 	private Button saveButton;
 	private Button viewButton;
+	private Menu myMenu;
 	
 	//Database object
 	private WeatherStationDB weatherDB;
@@ -157,21 +160,35 @@ public class MainActivity extends Activity implements SensorEventListener, Locat
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
         return true;
     }
+    
+   
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId())
+        {
+        	case R.id.start:
+        		//method();
+        		return true;
+    		
+        	case R.id.stop:
+        		//method();
+        		return true;
+        		
+    		default:
+    			return super.onOptionsItemSelected(item);
+        } 
     }
+    
+    
     @Override
     protected void onResume() {
       super.onResume();
